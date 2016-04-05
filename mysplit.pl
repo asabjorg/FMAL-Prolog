cdb40be2d0dd147944768139e113f5062f4987ea
Ása Björg Guðlaugsdóttir <asa13@ru.is>
2016-04-05 20:50:49 +0000

    ..

--- mysplit.pl
+++ mysplit.pl
@@ -1,5 +1,6 @@
 mysplit([],[],[]).
-mysplit(L2,[],L2).
 mysplit(L1,L1,[]).
-mysplit([[H1,H2]|L1],[H3|L2],[H4|L3]):- H1 = H3, H2 = H4, mysplit(L1,L2,L3).
+mysplit(L1,[],L1).
+mysplit([H1, H2], H1,H2).
+mysplit([H1,H2|L1],[H1|L2],[H2|L3]):- mysplit(L1,L2,L3).
 
