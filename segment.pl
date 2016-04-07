9daff5da20037e5d39c7020bb1f0bd3753a65aa9
brynhildurb12 <brynhildurb12@ru.is>
2016-04-07 10:42:11 -0400

    2d komið

--- segment.pl
+++ segment.pl
@@ -1,3 +1,7 @@
-segment([2,3,4],[1,2,3,4]).
+segment([],_).
+segment([X],[H2|_]) :- X == H2.
+segment([H1,H2|X],[H3,H4|Y]) :- H1 == H3, H2 == H4, segmentHelper(X,Y),!.
+segment([H1|X],[_|Y]) :- segment([H1|X],Y).
 
-segment([],Y).
+segmentHelper([],_).
+segmentHelper([H|X],[H|Y]) :- segmentHelper(X,Y).
